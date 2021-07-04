bae1203166505a4e4d163e51dbaffe081c9b50e1
Taki-88 <86900873+Taki-88@users.noreply.github.com>
2021-07-04 12:00:56 +0600

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR BOT'S TOKEN HERE")                
+client.run("ODYxMTIxNTkwMjM4NTExMTI0.YOFL-Q.7Xv8S6SV3TW21HeVPeq9r2Nyncw")                
